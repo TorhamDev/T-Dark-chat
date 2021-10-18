931712a2ecd784f2327b2d0d09b336ff4519b949
TorhamDEV <torhamdev@gmail.com>
2021-10-18 19:12:16 +0330

    create users database

--- DarkChat/api/models.py
+++ DarkChat/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User_code(models.Model):
+    '''
+    Dark chat users database model
+    '''
+
+    username_code = models.CharField(max_length=25, unique=True)
+    username_code_pass = models.CharField(max_length=50)
+
+
+    def __str__(slef) -> str:
+
+        return slef.username_code
+    
