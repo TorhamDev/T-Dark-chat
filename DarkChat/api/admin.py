a6eab89f2e154007299fe88cb447db56fc3f4803
TorhamDEV <torhamdev@gmail.com>
2021-10-19 15:15:25 +0330

    add message model database in admin panel

--- DarkChat/api/admin.py
+++ DarkChat/api/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from api.models import User_code
+from api.models import User_code, messages
+
 # Register your models here.
 
 
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ("message_text", "date", "update_date")
 
-admin.site.register(User_code)
+
+class User_codeAdmin(admin.ModelAdmin):
+    list_display = ("username_code", "create_date", "update_date")
+
+
+admin.site.register(User_code, admin_class=User_codeAdmin)
+admin.site.register(messages, admin_class=MessagesAdmin)
