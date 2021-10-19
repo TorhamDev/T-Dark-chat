03bf889cbc84bdcdd488f26288f17fee8f59e762
TorhamDEV <torhamdev@gmail.com>
2021-10-19 15:14:10 +0330

    edit user_code model and create message database model

--- DarkChat/api/models.py
+++ DarkChat/api/models.py
@@ -9,8 +9,21 @@ class User_code(models.Model):
     username_code = models.CharField(max_length=25, unique=True)
     username_code_pass = models.CharField(max_length=50)
 
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
-    def __str__(slef) -> str:
 
-        return slef.username_code
+
+class messages(models.Model):
+    '''
+    user messages database model
+    '''
+
+    message_text = models.TextField()
+
+    sender_user = models.ForeignKey("User_code", on_delete=models.CASCADE, related_name="sender")
+    receiver_user = models.ForeignKey("User_code", on_delete=models.CASCADE, related_name="receiver")
+
+    date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     
