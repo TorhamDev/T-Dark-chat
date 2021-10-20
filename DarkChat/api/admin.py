dabc5cc99003372e47084e7ade044f00c7af71cf
TorhamDEV <torhamdev@gmail.com>
2021-10-20 12:33:48 +0330

    register Valid_codes  model in admin panel

--- DarkChat/api/admin.py
+++ DarkChat/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import User_code, messages
+from api.models import User_code, Messages, Validـcodes
 
 # Register your models here.
 
@@ -12,5 +12,10 @@ class User_codeAdmin(admin.ModelAdmin):
     list_display = ("username_code", "create_date", "update_date")
 
 
+class Valid_codeAdmin(admin.ModelAdmin):
+    list_display = ("valid_code", "is_valid","create_date", "update_date")
+
+
 admin.site.register(User_code, admin_class=User_codeAdmin)
-admin.site.register(messages, admin_class=MessagesAdmin)
+admin.site.register(Messages, admin_class=MessagesAdmin)
+admin.site.register(Validـcodes, admin_class=Valid_codeAdmin)
