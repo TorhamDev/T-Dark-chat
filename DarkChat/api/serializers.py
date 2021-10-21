ae4033df20c043c3cf26558e9b1a602b4ec2e248
TorhamDEV <torhamdev@gmail.com>
2021-10-21 14:32:56 +0330

    add UserCodesSerializer class

--- DarkChat/api/serializers.py
+++ DarkChat/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Validـcodes, Messages
+from api.models import Validـcodes, Messages,User_code
 
 
 class ValidCodesSerializer(serializers.ModelSerializer):
@@ -14,3 +14,9 @@ class MessagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Messages
         fields = ["message_text", "sender", "receiver", "date"]
+
+
+class UserCodesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_code
+        fields = ["username_code", "create_date"]
