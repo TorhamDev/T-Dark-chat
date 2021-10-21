b7c3e485b044ee6774da86213665a7888267302c
TorhamDEV <torhamdev@gmail.com>
2021-10-21 14:33:59 +0330

    add path for set password api

--- DarkChat/api/urls.py
+++ DarkChat/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 urlpatterns = [
     path('',views.index),
     path('api/v1/register/get-valid-code/', views.GetValid_Code.as_view(), name="get valid code"),
+    path('api/v1/register/set-user-password/', views.SetUserPasswordCode.as_view(), name="set password"),
 
 ]
